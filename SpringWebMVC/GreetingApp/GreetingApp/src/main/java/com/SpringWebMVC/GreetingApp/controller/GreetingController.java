cc3d7532ba3cc026f7236788281d2744e30ec929
Sai Gopinath <sainath.gopinath@gmail.com>
2024-11-12 16:35:23 -0500

    SpringMVC: ObjectsInJSP example

--- SpringWebMVC/GreetingApp/GreetingApp/src/main/java/com/SpringWebMVC/GreetingApp/controller/GreetingController.java
+++ SpringWebMVC/GreetingApp/GreetingApp/src/main/java/com/SpringWebMVC/GreetingApp/controller/GreetingController.java
@@ -1,5 +1,8 @@
 package com.SpringWebMVC.GreetingApp.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.SpringWebMVC.GreetingApp.service.IGreetings;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+//http://localhost:8484/GreetingApp/wish
+//http://localhost:8484/GreetingApp/greet
 
 @Controller
 public class GreetingController 
@@ -19,6 +26,15 @@ public class GreetingController
 		String message = GreetingService.generateWish("Abc");
 		model.addAttribute("wish", message);
 		return "greeting";
+	}
+	
+	@GetMapping("/wish")
+	public void generateWish2(HttpServletResponse response) throws IOException
+	{
+		String greeting=GreetingService.generateWish("Writer");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello this response is from controller directly"+greeting+"</h1>");
+	
 	}
 
 }
